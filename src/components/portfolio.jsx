00541794077615d8f57d476e7f5f4ae9a5d5e3ee
andre_viallon <viallon.andre@gmail.com>
2020-07-20 17:38:07 +0200

    Fix error that would prevent deploy

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -13,21 +13,21 @@ const projects = [
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.',
         websiteLink: 'https://infinite-crag-46049.herokuapp.com/',
         githubLink: 'https://github.com/andreviallon/event-app',
-        imageId: "54898cee-6c8f-5bfb-a8b3-bf5f4711a406"
+        imageName: "eventux-thumbnail"
     },
     {
         title: 'Todo App',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.',
         websiteLink: 'https://infinite-crag-46049.herokuapp.com/',
         githubLink: 'https://github.com/andreviallon/react-node-todo-app',
-        imageId: "a497b465-d2a1-5f24-a53b-db77a1268cab"
+        imageName: "todo-app-thumbnail"
     },
     {
         title: 'Weather App',
         description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.',
         websiteLink: 'https://jovial-williams-ca9530.netlify.app/',
         githubLink: 'https://github.com/andreviallon/weather-app',
-        imageId: "492805b5-bac2-5ed5-a407-81035dee6fdb"
+        imageName: "weather-app-thumbnail"
     }
 ]
 
@@ -36,7 +36,6 @@ export default function Portfolio() {
         query Images {
             images: allFile(filter: {relativeDirectory: {eq: "thumbnails"}}) {
                 nodes {
-                    id
                     name
                     childImageSharp {
                         fluid {
@@ -78,9 +77,8 @@ export default function Portfolio() {
         });
     });
 
-    const getImageData = (id) => {
-        const image = data.images.nodes.filter(image => image.id === id);
-        console.log('data', data);
+    const getImageData = (imageName) => {
+        const image = data.images.nodes.filter(image => image.name === imageName);
         return image[0].childImageSharp.fluid;
     }
 
@@ -94,7 +92,7 @@ export default function Portfolio() {
                     <Row gutterWidth={70}>
                         {projects.map((project, index) => (
                             <Col sm={12} md={12} lg={6} key={index}>
-                                <PortfolioCard project={project} image={getImageData(project.imageId)}></PortfolioCard>
+                                <PortfolioCard project={project} image={getImageData(project.imageName)}></PortfolioCard>
                             </Col>
                         ))}
                     </Row>
