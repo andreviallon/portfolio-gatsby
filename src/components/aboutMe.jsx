b29a12357d25ef83042cd7bd8ace5cde39fa3b44
andre_viallon <viallon.andre@gmail.com>
2020-10-04 13:00:59 +0200

    fix typos

--- src/components/aboutMe.jsx
+++ src/components/aboutMe.jsx
@@ -64,9 +64,9 @@ export default function AboutMe() {
                     <Row gutterWidth={100}>
                         <Col md={12} lg={8}>
                             <div className="about-description" ref={el => content = el}>
-                                <p>So, this is where I get to talk a bit about myself. My name is André. I’m a Frontend Developer with a great passion for creating beautiful, modern and intuitive User Iterfaces.</p>
-                                <p>I’ve always watned to push myself and learn more. That’s why I left France, my home country, to move to Denmark back in 2012 where I joined the International Baccalaureate in Sønderborg. I then went on with studying Web Development as well as UI desing at the International Business Academy in Kolding.</p>
-                                <p>In my spare time I play the guitar (since the age of 12). The main thing playing a musical instrument taught me is that nothing comes easily. The only way to success is through dedication, dicipline and hard work.</p>
+                                <p>So, this is where I get to talk a bit about myself. My name is André. I’m a Frontend Developer with a great passion for creating beautiful, modern and intuitive User Interfaces.</p>
+                                <p>I’ve always wanted to push myself and learn more. That’s why I left France, my home country, to move to Denmark back in 2012 where I joined the International Baccalaureate in Sønderborg. I then went on with studying Web Development as well as UI design at the International Business Academy in Kolding.</p>
+                                <p>In my spare time I play the guitar (since the age of 12). The main thing playing a musical instrument taught me is that nothing comes easily. The only way to success is through dedication, discipline and hard work.</p>
                                 <p>I encourage you to checkout my resume to learn more about my past experience and my skills.</p>
                             </div>
                             <div className="open-resume-container" ref={el => resume = el}>
